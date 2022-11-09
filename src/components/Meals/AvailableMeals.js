51255f25b9d088998d1b0335b1cace1de768448e
shashank <shashankk1233.sk@gmail.com>
2022-11-09 17:20:19 +0530

    tutorial project completed

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -30,12 +30,12 @@ const DUMMY_MEALS = [
   
 const AvailableMeals = ()=>{
     const mealsList = DUMMY_MEALS.map(meal=>
-    <MealItem key={meal.id} 
+    <MealItem key={meal.id}
+    id={meal.id} 
     name={meal.name} 
     description={meal.description}
     price={meal.price}
     />);
-
     return <section className={classes.meals}>
       <Card>
         <ul>
